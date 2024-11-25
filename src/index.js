168b2dc86f54956a977e5de8ccf677053fa67535
Pedro <phoavl07@gmail.com>
2024-11-25 11:03:35 -0300

    Configura react-router-dom, history e axios

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './Components/App';
 import ClienteModify from './Components/Cliente/ClienteModify';
 import 'semantic-ui-css/semantic.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router-dom';
+import history from './history';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <App />} />
-        <Route path="/cliente/novo/" element={ <ClienteModify /> } />
-        <Route path="/cliente/alterar/:id" element={ <ClienteModify /> } />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+/*
+ReactDOM.render(
+    <Router history={history}>
+        <Route path="/" exact={true} component={App} />
+        <Route path="/album/" exact={true} component={AlbumCrud} />
+        <Route path="/album/consultar/:id" exact={true} component={AlbumRead} />
+        <Route path="/album/novo/" exact={true} component={AlbumModify} />
+        <Route path="/album/alterar/:id" exact={true} component={AlbumModify} />
+        <Route path="/album/deletar/:id" exact={true} component={AlbumDelete} />
+    </Router>
+    , document.querySelector("#root"));
+    */
+
+ReactDOM.render(
+      <Router history={history}>
+        <Route path="/" exact={true}  component={App} />
+        <Route path="/cliente/novo/" exact={true}  component={ClienteModify} />
+        <Route path="/cliente/alterar/:id" exact={true}  component={ClienteModify} />
+      </Router>, document.querySelector("#root")
 );
 

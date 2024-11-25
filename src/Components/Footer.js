353bd517f41a2d871dcb227f409360d78f379f33
Pedro <phoavl07@gmail.com>
2024-11-25 11:04:26 -0300

    Home Page e Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import "../App.css"
+
+function Footer(){
+    return(
+        <footer>
+            <div>Loja CupCakes</div>
+            
+            <h3>Copyright 2024, LojaCC Ltd.</h3>
+        </footer>
+    )
+
+}
+
+export default Footer;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,36 @@
 import React from "react";
+//import api from './apis';
+import brigadeiro from "../cup_brigadeiro.png"
+import verde from "../cupcake_verde.jpg"
+import Footer from "./Footer";
 
 const Home = () => {
-    <h1>Home</h1>;
+    /*api.get("/api/cliente")
+            .then(result => {
+                console.log(result)
+                }
+            );*/
+    return(
+        <div>
+            <div className="btnsUserCard">
+                <i style={{margin: '0 15px'}} className="shopping cart icon"></i>
+                <i style={{margin: '0 15px'}} className="user plus icon"></i>
+                <a href={"cliente/novo"}>Cadastre-se</a>
+            </div>
+            <div style={{margin: '20px'}} className="ui container">
+                <h1>LOJA CUP CAKES</h1>
+            </div>
+            <div>
+                <a href={"https://google.com"} className="ui medium image">
+                    <img src={brigadeiro} alt="Cup Cake Brigadeiro" />
+                </a>
+                <a href={"https://google.com"} className="ui medium image">
+                    <img src={verde} alt="Cup Cake Verde" />
+                </a>
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default Home;
